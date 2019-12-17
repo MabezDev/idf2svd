f95a4b77fb1b3373af5e40d6e5c4549963e51345
Scott Mabin <scott@mabez.dev>
2019-12-17 23:34:22 +0000

    Start parsing, add struct definitions for peripherals and registers

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,74 @@
-/* Regex to find all the peripheral base addresses */
-pub const REG_BASE: &'static str = r"\#define[\s*]+DR_REG_(.*)_BASE[\s*]+0x([0-9a-fA-F]+)";
 
-/*  */
+use std::ops::RangeInclusive;
+use std::collections::HashMap;
+
+
+/* Regex's to find all the peripheral addresses */
+pub const REG_BASE: &'static str = r"\#define[\s*]+DR_REG_(.*)_BASE[\s*]+0x([0-9a-fA-F]+)";
 pub const REG_DEF: &'static str = r"\#define[\s*]+([^\s*]+)[\s*]+\(DR_REG_(.*)_BASE \+ (.*)\)";
 pub const REG_DEF_INDEX: &'static str =
-    r"\#define[\s*]+([^\s*]+)[\s*]+\(([0-9A-Za-z_]+_BASE)[\s*]*\(i\) \+ (.*)\)";
+    r"\#define[\s*]+([^\s*]+)[\s*]+\(REG_([0-9A-Za-z_]+)_BASE[\s*]*\(i\) \+ (.*)\)";
 pub const REG_BITS: &'static str =
     r"#define[\s*]+([^\s*]+)_(S|V)[\s*]+\(?(0x[0-9a-fA-F]+|[0-9]+)\)?";
-
+#[derive(Debug, Default)]
 pub struct Peripheral {
-    name: String,
-    address: u32,
+    pub description: String,
+    pub address: u32,
+    pub registers: Vec<Register>,
 }
+
+#[derive(Debug, Default)]
+pub struct Register {
+    /// Register Name
+    pub name: String,
+    /// Relative Address
+    pub address: u32,
+    /// Width
+    pub width: u8,
+    /// Description
+    pub description: String,
+    /// Reset Value
+    pub reset_value: u64,
+    /// Detailed description
+    pub detailed_description: Option<String>,
+    pub bit_fields: Vec<BitField>,
+}
+
+#[derive(Debug, Default)]
+pub struct BitField {
+    /// Field Name
+    pub name: String,
+    /// Bits
+    pub bits: Bits,
+    /// Type
+    // pub type_: Type,
+    /// Reset Value
+    pub reset_value: u32,
+    /// Description
+    pub description: String,
+}
+
+#[derive(Debug)]
+pub enum Bits {
+    Single(u8),
+    Range(RangeInclusive<u8>),
+}
+
+impl Default for Bits {
+    fn default() -> Self {
+        Bits::Single(0)
+    }
+}
+
+// #[derive(Debug)]
+// pub enum Type {
+//     ReadAsZero,
+//     ReadOnly,
+//     ReadWrite,
+//     ReadWriteSetOnly,
+//     ReadableClearOnRead,
+//     ReadableClearOnWrite,
+//     WriteAsZero,
+//     WriteOnly,
+//     WriteToClear,
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 pub const SOC_BASE_PATH: &'static str = "esp-idf/components/soc/esp32/include/soc/";
 
-use header2svd::{REG_BASE, REG_BITS, REG_DEF, REG_DEF_INDEX};
+use header2svd::{REG_BASE, REG_BITS, REG_DEF, REG_DEF_INDEX, Peripheral, Register, BitField};
 use regex::Regex;
-use std::fs::{DirEntry, File};
+use std::fs::File;
 use std::io::prelude::*;
+use std::collections::HashMap;
 
 fn main() {
-    // let registers = Hashmap::new();
+    let mut peripherals = HashMap::new();
 
     let filname = SOC_BASE_PATH.to_owned() + "soc.h";
     let re_base = Regex::new(REG_BASE).unwrap();
@@ -15,9 +16,14 @@ fn main() {
     let re_reg_bits = Regex::new(REG_BITS).unwrap();
     
 
-    for caps in re_base.captures_iter(file_to_string(&filname).as_str()) {
-        // println!("Found: {:?}", caps);
-        // TODO load up
+    for captures in re_base.captures_iter(file_to_string(&filname).as_str()) {
+        let peripheral = &captures[1];
+        let address = &captures[2];
+        let mut p = Peripheral::default();
+        p.address = u32::from_str_radix(address, 16).unwrap();
+        p.description = peripheral.to_string();
+
+        peripherals.insert(peripheral.to_string(), p);
     }
 
     std::fs::read_dir(SOC_BASE_PATH)
@@ -27,31 +33,52 @@ fn main() {
         .for_each(|f| {
             let name = f.path();
             let name = name.to_str().unwrap();
-            println!("Searching file: {}", name);
+            // println!("Searching file: {}", name);
             let file_data = file_to_string(name);
 
             /* Normal register definitions */
             for register in re_reg.captures_iter(file_data.as_str()) {
-                match (&register[1], &register[2], &register[3]) {
-                    // TODO, running cargo run | grep "Normal" | grep "(i)", a few slip through, we need to handle that
-                    (reg_name, pname, offset) => {
-                        println!("Normal: {} @ {}", reg_name, offset);
-                    }
-                    _ => {}
+                let reg_name = &register[1];
+                let pname = &register[2];
+                let offset = &register[3].trim_start_matches("0x");
+                if reg_name.ends_with("(i)") { // some indexed still get through, ignore them
+                    continue;
+                }
+                if let Ok(addr) = u32::from_str_radix(offset, 16) {
+                    let p = peripherals.get_mut(&pname.to_string()).unwrap();
+                    let mut r = Register::default();
+                    r.description = reg_name.to_string();
+                    r.name = reg_name.to_string();
+                    r.address = addr;
+                    p.registers.push(r);
+                } else {
+                    println!("Failed to parse register for {}: {}", reg_name, offset)
                 }
             }
 
             /* Indexed register definitions */
             for register in re_reg_index.captures_iter(file_data.as_str()) {
-                match (&register[1], &register[2], &register[3]) {
-                    (reg_name, pname, offset) => {
-                        println!("Indexed: {} @ {}", reg_name, offset);
+                let reg_name = &register[1];
+                let pname = &register[2];
+                let offset = &register[3].trim_start_matches("0x");
+                if let Ok(addr) = u32::from_str_radix(offset, 16) {
+                    if let Some(p) = peripherals.get_mut(&pname.to_string()) {
+                        let mut r = Register::default();
+                        r.description = reg_name.to_string();
+                        r.name = reg_name.to_string();
+                        r.address = addr;
+                        p.registers.push(r);
+                    } else {
+                        println!("No periphal called {}", pname.to_string());
                     }
-                    _ => {}
+                } else {
+                    println!("Failed to parse register for {}: {}", reg_name, offset)
                 }
             }
 
         });
+
+        println!("{:#?}", peripherals);
 }
 
 
