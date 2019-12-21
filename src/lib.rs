1fd093ca2153ac557b13428cee8997890fd5a0f9
Scott Mabin <scott@mabez.dev>
2019-12-21 12:43:53 +0000

    Small refactor, casually fix some undefined behaviour.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+use regex::Regex;
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::prelude::*;
 use std::ops::RangeInclusive;
 
 /* Regex's to find all the peripheral addresses */
@@ -72,3 +76,162 @@ impl Default for Bits {
 //     WriteOnly,
 //     WriteToClear,
 // }
+
+enum State {
+    FindReg,
+    FindBitFieldInfo(String, Register),
+    FindDescription(String, Register, BitField),
+    CheckEnd(String, Register),
+}
+
+pub fn parse_idf(path: &str) -> HashMap<String, Peripheral> {
+    let mut peripherals = HashMap::new();
+
+    let filname = path.to_owned() + "soc.h";
+    let re_base = Regex::new(REG_BASE).unwrap();
+    let re_reg = Regex::new(REG_DEF).unwrap();
+    let re_reg_index = Regex::new(REG_DEF_INDEX).unwrap();
+    let re_reg_desc = Regex::new(REG_DESC).unwrap();
+    let re_reg_bit_info = Regex::new(REG_BIT_INFO).unwrap();
+    // let re_reg_bits = Regex::new(REG_BITS).unwrap();
+
+    for captures in re_base.captures_iter(file_to_string(&filname).as_str()) {
+        let peripheral = &captures[1];
+        let address = &captures[2];
+        let mut p = Peripheral::default();
+        p.address = u32::from_str_radix(address, 16).unwrap();
+        p.description = peripheral.to_string();
+
+        peripherals.insert(peripheral.to_string(), p);
+    }
+
+    std::fs::read_dir(path)
+        .unwrap()
+        .filter_map(Result::ok)
+        .filter(|f| f.path().to_str().unwrap().ends_with("_reg.h"))
+        .for_each(|f| {
+            let name = f.path();
+            let name = name.to_str().unwrap();
+            let mut buffer = vec![];
+            let file_data = file_to_string(name);
+            println!("Searching {}", name);
+
+            let mut state = State::FindReg;
+            for line in file_data.lines() {
+                loop {
+                    match state {
+                        State::FindReg => {
+                            /* Normal register definitions */
+                            if let Some(m) = re_reg.captures(line) {
+                                let reg_name = &m[1];
+                                let pname = &m[2];
+                                let offset = &m[3].trim_start_matches("0x");
+                                if reg_name.ends_with("(i)") {
+                                    // some indexed still get through, ignore them
+                                    break;
+                                }
+                                if let Ok(addr) = u32::from_str_radix(offset, 16) {
+                                    let mut r = Register::default();
+                                    r.description = reg_name.to_string();
+                                    r.name = reg_name.to_string();
+                                    r.address = addr;
+                                    state = State::FindBitFieldInfo(pname.to_string(), r);
+                                } else {
+                                    println!(
+                                        "Failed to parse register for {}: {}",
+                                        reg_name, offset
+                                    )
+                                }
+                            } else if let Some(m) = re_reg_index.captures(line) {
+                                let reg_name = &m[1];
+                                let pname = &m[2];
+                                let offset = &m[3].trim_start_matches("0x");
+
+                                if let Ok(addr) = u32::from_str_radix(offset, 16) {
+                                    let mut r = Register::default();
+                                    r.description = reg_name.to_string();
+                                    r.name = reg_name.to_string();
+                                    r.address = addr;
+                                    state = State::FindBitFieldInfo(pname.to_string(), r);
+                                } else {
+                                    println!(
+                                        "Failed to parse register for {}: {}",
+                                        reg_name, offset
+                                    )
+                                }
+                            }
+                            break; // next line
+                        }
+                        State::FindBitFieldInfo(ref mut pname, ref mut reg) => {
+                            if let Some(m) = re_reg_bit_info.captures(line) {
+                                let bf_name = &m[1];
+                                let _access_type = &m[2]; // TODO
+                                let bits = &mut m[3].split(':');
+                                let _default_val = &m[4]; // TODO
+                                let bits = match (bits.next(), bits.next()) {
+                                    (Some(h), Some(l)) => {
+                                        Bits::Range(l.parse().unwrap()..=h.parse().unwrap())
+                                    }
+                                    (Some(b), None) => Bits::Single(b.parse().unwrap()),
+                                    _ => {
+                                        println!("Failed to parse bitpos {}", &m[3]);
+                                        continue;
+                                    }
+                                };
+
+                                let bf = BitField {
+                                    name: bf_name.to_string(),
+                                    bits,
+                                    reset_value: 0,
+                                    ..Default::default()
+                                };
+                                state = State::FindDescription(pname.clone(), reg.clone(), bf);
+                            } else {
+                                println!("Failed to match reg info");
+                                state = State::FindReg;
+                            }
+                            break; // next line
+                        }
+                        State::FindDescription(ref mut pname, ref mut reg, ref mut bf) => {
+                            buffer.push(line);
+                            if let Some(m) = re_reg_desc.captures(buffer.join("").as_str()) {
+                                bf.description = m[1].to_string();
+                                buffer.clear();
+                                reg.bit_fields.push(bf.clone()); // add the bit field to the reg
+                                state = State::CheckEnd(pname.clone(), reg.clone());
+                            }
+                            break; // next line
+                        }
+                        State::CheckEnd(ref mut pname, ref mut reg) => {
+                            if line.is_empty() {
+                                // println!("{} Adding {:#?}", pname, reg);
+                                // were done with this register
+                                if let Some(p) = peripherals.get_mut(&pname.to_string()) {
+                                    p.registers.push(reg.clone());
+                                } else {
+                                    // TODO indexed peripherals wont come up here
+                                    println!("No periphal called {}", pname.to_string());
+                                }
+                                state = State::FindReg;
+                                break; // next line
+                            } else if re_reg_bit_info.is_match(line) {
+                                // weve found the next bit field in the reg
+                                state = State::FindBitFieldInfo(pname.clone(), reg.clone());
+                            } else {
+                                break; // next line
+                            }
+                        }
+                    }
+                }
+            }
+        });
+
+    peripherals
+}
+
+fn file_to_string(fil: &str) -> String {
+    let mut soc = File::open(fil).unwrap();
+    let mut data = String::new();
+    soc.read_to_string(&mut data).unwrap();
+    data
+}
--- src/main.rs
+++ src/main.rs
@@ -1,178 +1,38 @@
 pub const SOC_BASE_PATH: &'static str = "esp-idf/components/soc/esp32/include/soc/";
 
-use header2svd::{
-    BitField, Bits, Peripheral, Register, REG_BASE, REG_BITS, REG_BIT_INFO, REG_DEF, REG_DEF_INDEX,
-    REG_DESC,
-};
-use regex::Regex;
+use header2svd::{parse_idf, Bits, Peripheral};
+
 use std::collections::HashMap;
 use std::fs::File;
-use std::io::prelude::*;
+use std::io::BufWriter;
 use std::mem;
 use std::ptr;
 use svd_parser::{
     bitrange::BitRangeType, encode::Encode, BitRange, Field, FieldInfo,
     Peripheral as SvdPeripheral, Register as SvdRegister, RegisterCluster, RegisterInfo,
+    RegisterProperties,
 };
 use xmltree::Element;
 
-enum State {
-    FindReg,
-    FindBitFieldInfo(String, Register),
-    FindDescription(String, Register, BitField),
-    CheckEnd(String, Register),
-}
-
 fn main() {
-    let mut peripherals = HashMap::new();
+    let peripherals = parse_idf(SOC_BASE_PATH);
 
-    let filname = SOC_BASE_PATH.to_owned() + "soc.h";
-    let re_base = Regex::new(REG_BASE).unwrap();
-    let re_reg = Regex::new(REG_DEF).unwrap();
-    let re_reg_index = Regex::new(REG_DEF_INDEX).unwrap();
-    let re_reg_desc = Regex::new(REG_DESC).unwrap();
-    let re_reg_bit_info = Regex::new(REG_BIT_INFO).unwrap();
-    let re_reg_bits = Regex::new(REG_BITS).unwrap();
+    let svd = create_svd(peripherals).unwrap();
 
-    for captures in re_base.captures_iter(file_to_string(&filname).as_str()) {
-        let peripheral = &captures[1];
-        let address = &captures[2];
-        let mut p = Peripheral::default();
-        p.address = u32::from_str_radix(address, 16).unwrap();
-        p.description = peripheral.to_string();
-
-        peripherals.insert(peripheral.to_string(), p);
-    }
-
-    std::fs::read_dir(SOC_BASE_PATH)
-        .unwrap()
-        .filter_map(Result::ok)
-        .filter(|f| f.path().to_str().unwrap().ends_with("_reg.h"))
-        .for_each(|f| {
-            let name = f.path();
-            let name = name.to_str().unwrap();
-            let mut buffer = vec![];
-            let file_data = file_to_string(name);
-            println!("Searching {}", name);
-
-            let mut state = State::FindReg;
-            for line in file_data.lines() {
-                loop {
-                    match state {
-                        State::FindReg => {
-                            /* Normal register definitions */
-                            if let Some(m) = re_reg.captures(line) {
-                                let reg_name = &m[1];
-                                let pname = &m[2];
-                                let offset = &m[3].trim_start_matches("0x");
-                                if reg_name.ends_with("(i)") {
-                                    // some indexed still get through, ignore them
-                                    break;
-                                }
-                                if let Ok(addr) = u32::from_str_radix(offset, 16) {
-                                    let mut r = Register::default();
-                                    r.description = reg_name.to_string();
-                                    r.name = reg_name.to_string();
-                                    r.address = addr;
-                                    state = State::FindBitFieldInfo(pname.to_string(), r);
-                                } else {
-                                    println!(
-                                        "Failed to parse register for {}: {}",
-                                        reg_name, offset
-                                    )
-                                }
-                            } else if let Some(m) = re_reg_index.captures(line) {
-                                let reg_name = &m[1];
-                                let pname = &m[2];
-                                let offset = &m[3].trim_start_matches("0x");
-
-                                if let Ok(addr) = u32::from_str_radix(offset, 16) {
-                                    let mut r = Register::default();
-                                    r.description = reg_name.to_string();
-                                    r.name = reg_name.to_string();
-                                    r.address = addr;
-                                    state = State::FindBitFieldInfo(pname.to_string(), r);
-                                } else {
-                                    println!(
-                                        "Failed to parse register for {}: {}",
-                                        reg_name, offset
-                                    )
-                                }
-                            }
-                            break; // next line
-                        }
-                        State::FindBitFieldInfo(ref mut pname, ref mut reg) => {
-                            if let Some(m) = re_reg_bit_info.captures(line) {
-                                let bf_name = &m[1];
-                                let _access_type = &m[2]; // TODO
-                                let bits = &mut m[3].split(':');
-                                let _default_val = &m[4]; // TODO
-                                let bits = match (bits.next(), bits.next()) {
-                                    (Some(h), Some(l)) => {
-                                        Bits::Range(l.parse().unwrap()..=h.parse().unwrap())
-                                    }
-                                    (Some(b), None) => Bits::Single(b.parse().unwrap()),
-                                    _ => {
-                                        println!("Failed to parse bitpos {}", &m[3]);
-                                        continue;
-                                    }
-                                };
-
-                                let bf = BitField {
-                                    name: bf_name.to_string(),
-                                    bits,
-                                    reset_value: 0,
-                                    ..Default::default()
-                                };
-                                state = State::FindDescription(pname.clone(), reg.clone(), bf);
-                            } else {
-                                println!("Failed to match reg info");
-                                state = State::FindReg;
-                            }
-                            break; // next line
-                        }
-                        State::FindDescription(ref mut pname, ref mut reg, ref mut bf) => {
-                            buffer.push(line);
-                            if let Some(m) = re_reg_desc.captures(buffer.join("").as_str()) {
-                                bf.description = m[1].to_string();
-                                buffer.clear();
-                                reg.bit_fields.push(bf.clone()); // add the bit field to the reg
-                                state = State::CheckEnd(pname.clone(), reg.clone());
-                            }
-                            break; // next line
-                        }
-                        State::CheckEnd(ref mut pname, ref mut reg) => {
-                            if line.is_empty() {
-                                // println!("{} Adding {:#?}", pname, reg);
-                                // were done with this register
-                                if let Some(p) = peripherals.get_mut(&pname.to_string()) {
-                                    p.registers.push(reg.clone());
-                                } else {
-                                    // TODO indexed peripherals wont come up here
-                                    println!("No periphal called {}", pname.to_string());
-                                }
-                                state = State::FindReg;
-                                break; // next line
-                            } else if re_reg_bit_info.is_match(line) {
-                                // weve found the next bit field in the reg
-                                state = State::FindBitFieldInfo(pname.clone(), reg.clone());
-                            } else {
-                                break; // next line
-                            }
-                        }
-                    }
-                }
-            }
-        });
-
-    // println!("{:#?}", peripherals);
-    create_svd(peripherals).unwrap();
+    let f = BufWriter::new(File::create("esp32.svd").unwrap());
+    svd.write(f).unwrap();
 }
 
-fn create_svd(peripherals: HashMap<String, Peripheral>) -> Result<(), ()> {
+fn create_svd(peripherals: HashMap<String, Peripheral>) -> Result<Element, ()> {
     let mut svd_peripherals = vec![];
 
     for (name, p) in peripherals {
+        /*
+           Note on unsafe:
+           `mem::unitialized` is the only way to create these svd peripherals currently.
+           We must ensure that **all** the fields are initialized with a value
+           failure to do so will result in undefined behaviour when the value is dropped
+        */
         let mut out: SvdPeripheral = unsafe { mem::uninitialized() };
 
         let mut registers = vec![];
@@ -245,7 +105,17 @@ fn create_svd(peripherals: HashMap<String, Peripheral>) -> Result<(), ()> {
             ptr::write(&mut out.description, None);
             ptr::write(&mut out.base_address, p.address);
             ptr::write(&mut out.address_block, None);
+            // TODO parse interrupt information
             ptr::write(&mut out.interrupt, vec![]);
+
+            // TODO parse this information properly
+            let mut drp: RegisterProperties = mem::uninitialized();
+            ptr::write(&mut drp.access, None);
+            ptr::write(&mut drp.reset_mask, None);
+            ptr::write(&mut drp.reset_value, None);
+            ptr::write(&mut drp.size, None);
+
+            ptr::write(&mut out.default_register_properties, drp);
             ptr::write(&mut out.registers, Some(registers));
             ptr::write(&mut out.derived_from, None); // first.as_ref().map(|s| s.to_owned())
         }
@@ -267,14 +137,5 @@ fn create_svd(peripherals: HashMap<String, Peripheral>) -> Result<(), ()> {
         children: children,
         ..Element::new("device")
     };
-    let f = File::create("esp32.svd").unwrap();
-    out.write(f).unwrap();
-    Ok(())
-}
-
-fn file_to_string(fil: &str) -> String {
-    let mut soc = File::open(fil).unwrap();
-    let mut data = String::new();
-    soc.read_to_string(&mut data).unwrap();
-    data
+    Ok(out)
 }
