5c5452357711fd0d03c1fc178c2018293ab0f4e5
Scott Mabin <scott@mabez.dev>
2020-05-01 15:22:01 +0100

    Fix svd dep issue

--- src/common/mod.rs
+++ src/common/mod.rs
@@ -179,7 +179,11 @@ pub fn build_svd(
         let out = PeripheralBuilder::default()
             .name(name.to_owned())
             .base_address(p.address)
-            .registers(Some(registers))
+            .registers(if registers.is_empty() {
+                None
+            } else {
+                Some(registers)
+            })
             .address_block(Some(AddressBlock {
                 offset: 0x0,
                 size: block_size, // TODO what about derived peripherals?
